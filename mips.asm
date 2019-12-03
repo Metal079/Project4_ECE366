09a3e0c19e2f3398958322c616157f8870dfc5ad
Metal079 <Metal145965@gmail.com>
2019-12-03 06:56:21 -0600

    Part 1 done simulator works (need pipeline and cache)

--- mips.asm
+++ mips.asm
@@ -1,17 +1,27 @@
-addi $1, $0, 15
-sb $1, 0x2000($0)
-addi $2, $0, 4
-sw $2, 0x2000($2)
-lw $3, 0x2000($0)
-ori $4, $0, 24
-addi $5, $0, 32
-ori $6, $5, 24
-ori $7, $5, 48
-addu $8, $7, $5
-xor $8, $7, $8
-addi $10, $0, $1
-addi $9, $0, $1
-loop9:
-addi $9, $9, 1
-beq $9, $10, loop9
-bne $9, $2, loop9
+# Prog A, parameter = 24
+# version slt
+# asd
+# tayt
+ori $8, $0, 24
+addi $9, $0, 0x40
+sw_loop:
+sw $8, 0x2000($9)
+addi $9, $9, -4
+beq $9, $0, sw_done
+sll $10, $8, 24
+addu $10, $10, $8
+sub $8, $0, $8
+xor $8, $10, $8
+beq $0, $0, sw_loop
+sw_done:
+addi $10, $0, 0x40
+addu $12, $0, $0
+lw_loop:
+lw $8, 0x2000($9)
+slt $11, $8, $0
+bne $11, $0, skip
+addi $12, $12, 1
+skip:
+addi $9, $9, 4
+bne $9, $10, lw_loop
+sw $12, 0x2000($0) 
